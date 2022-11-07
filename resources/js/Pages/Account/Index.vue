5001c84aacefb122629b7519fa54601e902b2b41
puteprovod <helpyouchange@gmail.com>
2022-11-08 04:18:29 +0600

    SPA mode

---
+++ resources/js/Pages/Account/Index.vue
@@ -0,0 +1,27 @@
+<template>
+            <h1 class="text-lg mb-8">Posts</h1>
+            <div class="mb-8">
+                Blablabla
+            </div>
+
+</template>
+
+<script>
+import {Link} from "@inertiajs/inertia-vue3";
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+export default {
+    name: "index",
+    layout: MainLayout,
+    props: [
+        'accounts',
+    ],
+    components: {
+        Link
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
