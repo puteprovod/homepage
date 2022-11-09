faac07d3314eaaa284214ae7079a05bfd9f21ad4
puteprovod <helpyouchange@gmail.com>
2022-11-09 17:33:29 +0600

    SPA mode

---
+++ app/Http/Resources/Account/AccountResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Resources\Account;
+
+use App\Models\Account;
+use App\Models\Currency;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AccountResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $role=auth()->user() ? auth()->user()->role : '';
+        if ($role=='admin') {
+            return [
+                'id' => $this->id,
+                'title' => $this->title,
+                'value' => $this->value,
+                'cost' => $this->cost,
+                'category_title' => $this->category_title,
+                'currency_title' => $this->currency_title,
+                'cost_formatted' => $this->cost_formatted,
+                'category_id' => $this->category_id,
+                'image' => $this->image,
+                'comment' => $this->comment,
+            ];
+        }
+        else
+        {
+
+                $currency = Currency::find($this->currency_id);
+            ($currency->title=='BTC' || $currency->title=='ETH') ? $value = rand (0, 10) : $value = rand (0, 50000);
+            if ($this->category_id==24) $value=-$value;
+            $cost = 0;
+                if ($currency->source == 'cbr' or $currency->source == 'rub') {
+                    $cost = ceil($value * $currency->exchange_rate);
+                } else {
+                    $usd = Currency::find(11)->exchange_rate;
+                    $cost = ceil($value * $currency->exchange_rate * $usd);
+                }
+
+
+            return [
+                'id' => $this->id,
+                'title' => mb_substr($this->title,0,17,'UTF-8'),
+                'value' => $value,
+                'cost' => $cost,
+                'category_title' => $this->category_title,
+                'currency_title' => $this->currency_title,
+                'cost_formatted' => number_format($cost, 0, '.', " "),
+                'category_id' => $this->category_id,
+                'image' => $this->image,
+                'comment' => '',
+            ];
+        }
+    }
+}
