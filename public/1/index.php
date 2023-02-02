08c78658e5abd58390c57b8e4093722aa637b222
puteprovod <helpyouchange@gmail.com>
2023-02-02 16:59:36 +0600

    deploy

---
+++ public/1/index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<div>
+    <div style="display: inline-block; width: 30%">Цыплено1к</div>
+    <div style="display: inline-block">Отлично</div>
+</div>
+<div>
+    <div style="display: inline-block; width: 30%">Говядина2</div>
+    <div style="display: inline-block">Не пробовали</div>
+</div>
+</body>
+</html>
