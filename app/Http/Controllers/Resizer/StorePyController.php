ad1b463d0dd50d6beef81ad4be6c863b1a29eadd
puteprovod <helpyouchange@gmail.com>
2023-02-03 16:53:52 +0600

    deploy

--- app/Http/Controllers/Resizer/StorePyController.php
+++ app/Http/Controllers/Resizer/StorePyController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Resizer;
 use App\Http\Requests\Resizer\StoreRequest;
+use App\Models\ResizedImage;
 
 
 class StorePyController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-
+        ResizedImage::clearOldEntries();
         $data = $request->validated();
         return ($this->service->resizeImages($data));
 
--- app/Models/ResizedImage.php
+++ app/Models/ResizedImage.php
@@ -2,11 +2,35 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ResizedImage extends Model
 {
+    const SAVED_DAYS = 3; // COUNT OF DAYS WHILE IMAGES SAVED
+    public static function clearOldEntries () {
+        $currentTime = Carbon::now();
+        $expiryTime = $currentTime->subDays(self::SAVED_DAYS)->toDateTimeString();
+        $entries = DB::table('resized_images')
+            ->select('id','token','created_at')
+            ->where('created_at','<',$expiryTime)
+            ->groupBy('token')
+            ->get();
+        foreach ($entries as $entry) {
+            $token = $entry->token;
+            Storage::disk('public')->deleteDirectory('ResizedImages/' . $token);
+            Storage::disk('public')->deleteDirectory('ResizedImages/Previews/' . $token);
+            Storage::disk('public')->delete('ResizedImages/' . $token.'.json');
+        }
+        $entries = ResizedImage::All()->where('created_at','<',$expiryTime);
+        foreach ($entries as $entry){
+            ResizedImage::destroy($entry->id);
+        }
+    }
+
     protected $table = 'resized_images';
     protected $guarded = [];
     use HasFactory;
