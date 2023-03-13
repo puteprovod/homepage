ce4b13c88a0a6c459e903a67bae72444ef1ba035
puteprovod <helpyouchange@gmail.com>
2023-03-13 15:51:34 +0600

    Account.show

--- app/Http/Controllers/Admin/Account/ShowController.php
+++ app/Http/Controllers/Admin/Account/ShowController.php
@@ -9,6 +9,6 @@ class ShowController extends Controller
     public function __invoke(Account $account)
     {
         $accounts=Account::All();
-       return view('admin.Account.show', compact('account','accounts')); //Account
+       return view('admin.Account.show', compact('account','accounts')); //Account1
     }
 }
