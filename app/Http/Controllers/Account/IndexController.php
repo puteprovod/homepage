0a23af6e1569e0dda2920c6d1e67aabe8c4ef5c9
puteprovod <helpyouchange@gmail.com>
2023-04-04 19:19:12 +0600

    localize account titles

--- app/Http/Controllers/Account/IndexController.php
+++ app/Http/Controllers/Account/IndexController.php
@@ -17,10 +17,13 @@ class IndexController extends Controller
     public function __invoke()
     {
         Account::calculateCosts();
-
-        $accounts = Account::all();
-        $accounts = collect(AccountResource::collection($accounts)
-            ->resolve())->sortBy('category_title')->values()->toArray();
+        $accounts = DB::table('accounts')
+            ->join('categories', 'accounts.category_id', '=', 'categories.id')
+            ->join('currencies', 'accounts.currency_id', '=', 'currencies.id')
+            ->select('accounts.*', 'categories.title as category_title', 'currencies.title as currency_title')
+            ->orderBy('categories.title')
+            ->get();
+        $accounts = AccountResource::collection($accounts)->resolve();
         $saveDate = Carbon::now()->format('d.m.Y');
 
         $historyDatesCollection = AccountHistory::all()
